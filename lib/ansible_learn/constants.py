3a517864a29c2e559d2c1264ad4f88355cb6a00e
xiangxiaobaog3 <xiangxiaobaog3@gmail.com>
2017-08-25 19:11:21 +0800

    定义常量

--- lib/ansible_learn/constants.py
+++ lib/ansible_learn/constants.py
@@ -2,8 +2,8 @@
 
 # Added initial stub for where playbooks will go, moved to common
 
-DEFAULT_HOST_LIST = '/etc/ansible/hosts'
-DEFAULT_MODULE_PATH = '/usr/share/ansible'
+DEFAULT_HOST_LIST = '~/.ansible_hosts'
+DEFAULT_MODULE_PATH = '~/.ansible'
 DEFAULT_MODULE_NAME = 'ping'
 DEFAULT_PATTERN = '*'
 DEFAULT_FORKS = 3
